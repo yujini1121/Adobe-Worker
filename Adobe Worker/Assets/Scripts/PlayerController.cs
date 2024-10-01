f140b186cb75972769b55baad80095c5c94a13dc
yujini <yujini1121@naver.com>
2024-10-02 04:06:06 +0900

    Player Controller & Cinemachine Camera (3rd Person)
    - 3인칭 시점 카메라 작업
    - 플레이어 기본 움직임 작업

--- Adobe Worker/Assets/Scripts/PlayerController.cs
+++ Adobe Worker/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,89 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public CinemachineVirtualCamera virtualCam;
+    public GameObject virtualCamTarget;
+    private Rigidbody rb;
+
+    private float keyHorizontalAxisValue;
+    private float keyVerticalAxisValue;
+
+    [Header("Cinemachine Camera")]
+    private float virtualCamYaw;
+    private float virtualCamPitch;
+    [SerializeField] private float virtualCamPitchTop = 70.0f;
+    [SerializeField] private float virtualCamPitchBottom = -30.0f;
+
+    [Header("Player Move Value")]
+    private float targetRotation = 0f;
+    private float rotationVelocity = 0f;
+    [SerializeField] private float rotationSmoothTime = 0.12f;
+    [SerializeField] private float moveSpeed = 10.0f;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
+        Debug.LogWarning("Test : Mouse cursor is locked");
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        PlayerMove();
+    }
+
+    private void LateUpdate()
+    {
+        PlayerRotation();
+    }
+
+    void PlayerMove()
+    {
+        keyHorizontalAxisValue = Input.GetAxisRaw("Horizontal");
+        keyVerticalAxisValue = Input.GetAxisRaw("Vertical");
+
+        Vector3 inputDir = new Vector3(keyHorizontalAxisValue, 0.0f, keyVerticalAxisValue).normalized;
+
+        if (inputDir.sqrMagnitude > 0)
+        {
+            targetRotation = Mathf.Atan2(inputDir.x, inputDir.z) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
+            float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref rotationVelocity, rotationSmoothTime);
+
+            transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+        }
+
+        Vector3 targetDirection = Quaternion.Euler(0.0f, targetRotation, 0.0f) * Vector3.forward;
+
+        rb.velocity = targetDirection.normalized * (inputDir != Vector3.zero ? moveSpeed : 0.0f)
+                        + new Vector3(0.0f, rb.velocity.y, 0.0f);
+    }
+
+    void PlayerRotation()
+    {
+        Vector2 inputMousePos = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        if (inputMousePos.sqrMagnitude >= 0.01f) //지터링 방지
+        {
+            virtualCamYaw += inputMousePos.x;
+            virtualCamPitch += inputMousePos.y;
+        }
+
+        virtualCamYaw = ClampAngle(virtualCamYaw, float.MinValue, float.MaxValue);
+        virtualCamPitch = ClampAngle(virtualCamPitch, virtualCamPitchBottom, virtualCamPitchTop);
+
+        virtualCamTarget.transform.rotation = Quaternion.Euler(-virtualCamPitch, virtualCamYaw, 0.0f);
+    }
+
+    float ClampAngle(float lfAngle, float lfMin, float lfMax)
+    {
+        if (lfAngle < -360f) lfAngle += 360f;
+        if (lfAngle > 360f) lfAngle -= 360f;
+        return Mathf.Clamp(lfAngle, lfMin, lfMax);
     }
 }
