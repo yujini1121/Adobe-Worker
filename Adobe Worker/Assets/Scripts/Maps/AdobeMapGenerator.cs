737cc89ff7e96f0ebc379a86c64b08023a78d7a1
chocokart2 <chocokart@naver.com>
2024-10-22 12:42:43 +0900

    2024-10-22 맵 업데이트

--- Adobe Worker/Assets/Scripts/Maps/AdobeMapGenerator.cs
+++ Adobe Worker/Assets/Scripts/Maps/AdobeMapGenerator.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.Controls;
 
 public class AdobeMapGenerator : MonoBehaviour
 {
@@ -18,12 +19,18 @@ public class AdobeMapGenerator : MonoBehaviour
     [SerializeField] Vector2 mapSize;
     [SerializeField] int padding; // 경계 블록의 중심 지점에서부터 추가로 붙는 외부 블록 층수
     [SerializeField] int radius; //중심 지점에서 경계 블록의 중심 지점까지
-    //int middle = 
+    int middleX;
+    int middleY;
+    float radiusX;
+    float radiusY;
     bool[,] isInstantiated;
     int[,] mapPrefabIndexes;
+    List<(Vector2, GameObject)> instantiatedChunkList; // O(Xsize * Ysize)보다 더 줄일 수 있는 탐색을 위한 자료구조입니다.
+    Vector3 pivot;
 
-    System.Action<int, int> MapUpdateHandler;
-    
+    [Header("디버깅")]
+    [SerializeField] bool isDebuggingMethodCall;
+    [SerializeField] bool isDebuggingArrangeMap;
 
     // Start is called before the first frame update
     void Start()
@@ -39,8 +46,23 @@ public class AdobeMapGenerator : MonoBehaviour
     void Update()
     {
         // 매번 플레이어의 위치를 검사
+        // pivot 기준으로 엄청 멀어졌다 느끼면 ArrangeMap() 호출
+
+        if (IsNeedMapUpdate())
+        {
+            ArrangeMap();
+        }
 
         //MapUpdateHandler(1, 1);
+    }
+
+    bool IsNeedMapUpdate()
+    {
+        Vector3 position = AdobePlayerReference.playerInstance.transform.position;
+
+        if (Mathf.Abs((position - pivot).x) > radiusX * chunkSize.x) return true;
+        if (Mathf.Abs((position - pivot).z) > radiusY * chunkSize.y) return true;
+        return false;
     }
 
     void BakeMap()
@@ -77,27 +99,100 @@ public class AdobeMapGenerator : MonoBehaviour
 
     void ArrangeMap()
     {
-        
+        if (isDebuggingMethodCall)
+        {
+            Debug.Log("AdobeMapGenerator.ArrangeMap()");
+        }
+
+        // 자신의 위치 가져옴
+        Vector3 position = AdobePlayerReference.playerInstance.transform.position;
+        int posiitonIndexX = middleX + (int)Mathf.Floor((position.x - chunkSize.x / 2) / chunkSize.x);
+        int posiitonIndexY = middleY + (int)Mathf.Floor((position.y - chunkSize.y / 2) / chunkSize.y);
+
+        pivot = new Vector3(posiitonIndexX * chunkSize.x, 0, posiitonIndexY * chunkSize.y);
+
+        // 청크 생성
+        int m_size = 1 + radius * 2 + padding * 2;
+        for (int x = 0; x < m_size; ++x)
+        {
+            int xIndex = x - radius - padding + posiitonIndexX + 1;
+            if (xIndex < 0 || xIndex >= (int)mapSize.x) continue;
+
+            for (int y = 0; y < m_size; ++y)
+            {
+                int yIndex = y - radius - padding + posiitonIndexY + 1;
+                if (yIndex < 0 || yIndex >= (int)mapSize.y) continue;
+
+                if (isInstantiated[xIndex, yIndex]) continue;
+                isInstantiated[xIndex, yIndex] = true;
+
+                Vector3 generatePosition = new Vector3(
+                    (xIndex - middleX) * chunkSize.x,
+                    -0.5f,
+                    (yIndex - middleY) * chunkSize.y
+                    );
+
+                if (isDebuggingArrangeMap)
+                {
+                    Debug.Log($"AdobeMapGenerator.ArrangeMap() : {xIndex} / {yIndex}, 생성 좌표 : {generatePosition}");
+                }
+
+
+                GameObject m_chunk = Instantiate(
+                    chunkPrefab[mapPrefabIndexes[xIndex, yIndex]].prefab,
+                    generatePosition,
+                    chunkPrefab[mapPrefabIndexes[xIndex, yIndex]].prefab.transform.rotation
+                    );
+
+                instantiatedChunkList.Add((new Vector2(xIndex, yIndex), m_chunk));
+            }
+        }
+
+        // 멀어진 청크 잘라내기
+        for (int index = instantiatedChunkList.Count - 1; index >= 0; --index)
+        {
+            (Vector2 m_oneIndex, GameObject m_chunk) = instantiatedChunkList[index];
+
+            int x = Mathf.FloorToInt(m_oneIndex.x);
+            int y = Mathf.FloorToInt(m_oneIndex.y);
+
+
+        }
     }
 
     void InitField()
     {
-        isInstantiated = new bool[1 + radius * 2 + padding * 2, 1 + radius * 2 + padding * 2];
-        
-        for (int x = 0; x < isInstantiated.GetLength(0); ++x)
-        {
-            for (int y = 0; y < isInstantiated.GetLength(1); ++y)
-            {
-                bool m_value = true;
+        isInstantiated = new bool[(int)mapSize.x, (int)mapSize.y];
+        middleX = (int)(mapSize.x / 2);
+        middleY = (int)(mapSize.y / 2);
+        radiusX = chunkSize.x * radius + chunkSize.x / 2;
+        radiusY = chunkSize.y * radius + chunkSize.y / 2;
+        //isInstantiated = new bool[1 + radius * 2 + padding * 2, 1 + radius * 2 + padding * 2];
 
-                if (x < padding) m_value = false;
-                if (x > radius * 2 + padding) m_value = false;
-                if (y < padding) m_value = false;
-                if (y > radius * 2 + padding) m_value = false;
+        //for (int x = 0; x < isInstantiated.GetLength(0); ++x)
+        //{
+        //    for (int y = 0; y < isInstantiated.GetLength(1); ++y)
+        //    {
+        //        bool m_value = true;
 
-                isInstantiated[x, y] = m_value;
-            }
-        }
+        //        if (x < padding) m_value = false;
+        //        if (x > radius * 2 + padding) m_value = false;
+        //        if (y < padding) m_value = false;
+        //        if (y > radius * 2 + padding) m_value = false;
+
+        //        isInstantiated[x, y] = m_value;
+        //    }
+        //}
+
+        pivot = new Vector3(0, 0, 0);
+        instantiatedChunkList = new List<(Vector2, GameObject)>();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(pivot, 1);
+        Gizmos.color = Color.green;
+
 
     }
 }
